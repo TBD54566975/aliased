b61b0345b775c8bb00c82b25d51919d41a8ed2e8
Henry Tsai <henrytsai@outlook.com>
2024-09-12 23:47:49 -0700

    Fixed input border cropped issue in iOS

--- src/components/Onboarding.vue
+++ src/components/Onboarding.vue
@@ -3,7 +3,7 @@
     <div :key="currentStep" class="absolute inset-0 w-full h-full">
       <!-- Step 1 -->
       <div v-if="currentStep === 1">
-        <div class="h-screen flex flex-col justify-center items-center">
+        <div class="h-screen flex flex-col justify-center">
           <p class="my-6 text-4xl">Welcome to Aliased</p>
           <p class="my-2">Take control of your online identity and personal data.</p>
           <p class="my-2">Sign into apps with an identity you own and control.</p>
@@ -20,8 +20,11 @@
           <p class="my-2">A profile is a version of yourself online. When you connect to an app, you'll pick which profile to connect. Like email addresses, profiles are separate and not connected to one another.</p>
           <p class="my-2">You can always create, delete, and edit profiles later.</p>
 
+          <div class="my-2"></div>
+
           <!-- Profile Name Input -->
-          <div class="w-full max-w-lg mb-6">
+          <!-- p-1 is to handle border cropping in iOS/safari when input is selected -->
+          <div class="w-full max-w-lg mb-6 p-1">
             <label for="profile-name" class="block mb-2 font-semibold">Name your profile:</label>
             <input
               id="profile-name"
@@ -33,7 +36,7 @@
           </div>
 
           <!-- DWN Endpoint Input -->
-          <div class="w-full max-w-lg mb-6">
+          <div class="w-full max-w-lg mb-6 p-1">
             <label for="dwn-endpoint" class="block mb-2 font-semibold">DWN endpoint:</label>
             <input
               id="dwn-endpoint"
