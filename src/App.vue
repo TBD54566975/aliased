49763bfe693d83e748b4aa3555a2671e2cb6de6b
Henry Tsai <henrytsai@outlook.com>
2024-09-19 14:03:50 -0700

    Added graceful handling of deep link even if app session is not initialized

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,9 @@ const route = useRoute();
 // NOTE: There are two possible paths to initialize the app:
 // 1. Onboarding flow for first-time users
 // 2. Relaunching flow for returning users
-const initializationCompleted = ref(false);
+const sessionInitializationCompleted = ref(false);
 const hasExistingProfile = ref(false);
+const pendingDeepLink = ref<string | undefined>(undefined);
 
 // Determine if the current route is a modal route (e.g. /web5-connect-request)
 const isModalRoute = computed(() => route.name === 'web5-connect-request');
@@ -38,41 +39,59 @@ onMounted(async () => {
 
 const configureDeepLinkHook = () => {
   App.addListener('appUrlOpen', (data: any) => {
+    console.log(`Handling deep link of URL: ${data.url}...`);
     const url = data.url; // Get the full URL
-    console.log('App opened with URL:', url);
-
-    // Parse the deep link URL (web5://connect?request_uri=...)
-    if (url.startsWith('web5://connect')) {
-      const urlParams = new URLSearchParams(url.split('?')[1]);
-      const requestUri = urlParams.get('request_uri');
-      const encryptionKey = urlParams.get('encryption_key');
-
-      if (requestUri) {
-        console.log('Deep link request URI:', requestUri);
-        
-        // Trigger the web5 connect request handling page
-        router.push({
-          name: 'web5-connect-request', // The route for handling the connection
-          query: { request_uri: requestUri, encryption_key: encryptionKey }
-        });
-      }
+    
+    if (!sessionInitializationCompleted.value) {
+      console.log('User onboarding not completed yet or app PIN not given yet. Storing the deep link for later...');
+      pendingDeepLink.value = data.url;
+      return;
     }
+
+    // else continue with the deep link handling
+    handleDeepLink(url);
   });
 };
 
-const showProfilesPage = async () => {
-  initializationCompleted.value = true;
-  router.replace('/'); // Always land in the profiles page after initialization.
+const handleDeepLink = (url: string) => {
+  if (url.startsWith('web5://connect')) {
+    console.log('Handling Web5 connect deep link...');
+    const urlParams = new URLSearchParams(url.split('?')[1]);
+    const requestUri = urlParams.get('request_uri');
+    const encryptionKey = urlParams.get('encryption_key');
+
+    if (requestUri) {
+      console.log('Deep link request URI:', requestUri);
+      
+      // Trigger the web5 connect request handling page
+      router.push({
+        name: 'web5-connect-request', // The route for handling the connection
+        query: { request_uri: requestUri, encryption_key: encryptionKey }
+      });
+    }
+  }
+};
+
+// Method to be called after the onboarding or relaunching flows are completed.
+const onSessionInitialized = async () => {
+  sessionInitializationCompleted.value = true;
+  if (pendingDeepLink.value === undefined) {
+    router.replace('/'); // Always land in the profiles page after initialization.
+  } else {
+    console.log('Resuming handling of pending deep link:', pendingDeepLink.value);
+    handleDeepLink(pendingDeepLink.value!);
+    pendingDeepLink.value = undefined;
+  }
 };
 </script>
 
 <template>
   <!-- Handle the onboarding or relaunching flows -->
-  <Onboarding v-if="!initializationCompleted && !hasExistingProfile" @onboarding-complete="showProfilesPage" />
-  <Relaunching v-else-if="!initializationCompleted" @initialization-complete="showProfilesPage" />
+  <Onboarding v-if="!sessionInitializationCompleted && !hasExistingProfile" @onboarding-complete="onSessionInitialized" />
+  <Relaunching v-else-if="!sessionInitializationCompleted" @initialization-complete="onSessionInitialized" />
 
   <!-- Show main content after initialization is complete -->
-  <div v-if="initializationCompleted" class="flex flex-col h-screen">
+  <div v-if="sessionInitializationCompleted" class="flex flex-col h-screen">
 
     <!-- Main content rendered in the default RouterView -->
     <div class="flex-grow overflow-y-auto">
